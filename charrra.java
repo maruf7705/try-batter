5415d25928125d89394dfdf099d8068b975a7b18
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-07-25 12:00:38 +0600

    codeing

---
+++ charrra.java
@@ -0,0 +1,11 @@
+public class charrra {
+
+    public static void main(String[]args){
+        String name = "maruf";
+        String name2 = name.replace('a','b');
+        System.out.println(name2);
+        System.out.println(name2.length());
+        System.out.println(name2.substring(0, 3));
+    }
+    
+}
---
+++ yellow.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class yellow {
+
+    // public static void main(String[]args){
+    //     int age= 20;
+    //     int physics = 70;
+    //     int math = 90;
+    //     int eng = 100;
+    //     int[] marks = new int[3];
+    //     marks[0] = 80;
+    //     marks[1] = 90;
+    //     marks[2] = 99;
+    //     marks[3] = 97;
+    public static void main(String[]args){
+        // int age = 20;
+        // int math = 40;
+        // int eng =50;
+        // int physics = 40;
+        int[] marks = {90,49,49,3943,3903};
+        // marks[0] = 80;
+        // marks[1] = 77;
+        // marks[2] = 81;
+        // marks[3] = 100;
+        System.out.println(marks[0]);
+        Arrays.sort(marks);
+        System.out.println(marks[0]);
+    }
+    }
+    
+
