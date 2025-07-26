f00ee5c3fbf66c5a7616cc20326d699d0285c3c1
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-07-26 19:57:38 +0600

    update

---
+++ smiplepactice.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class smiplepactice {
+    public static void main(String[]args)
+    {
+        Scanner put = new Scanner(System.in);
+        
+    }
+}
--- whileloop.java
+++ whileloop.java
@@ -1,12 +1,25 @@
 public class whileloop {
-    public static void main(String[]args)
-    {
+    public static void main(String[]args){
+    // {
 
-       int j = 1;
-       while (j<=100) {
+    //    int j = 1;
+    //    while (j<=100) {
+    //     System.out.println(j);
+    //     j = j+1;
+    //    }
+
+    // }
+
+    for (int i =1 ; i<=100; i++ )
+    {
+        System.out.println(i);
+    }
+
+    int j = 1;
+    while (j<=100){
         System.out.println(j);
         j = j+1;
-       }
-
     }
+
+}
 }
