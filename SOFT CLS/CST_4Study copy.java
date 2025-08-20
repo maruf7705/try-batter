0249b01d1077731284c9c0b7e5dbe32ef5c5fd97
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-08-20 06:45:18 +0600

    update

---
+++ SOFT CLS/CST_4Study copy.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CST_4Study{
+    public static void main(String[]args){
+        // int c;
+        Scanner c = new Scanner(System.in);
+        System.out.print("What is the rediouse of your circle: ");
+        double r = c.nextDouble();
+        double area = 3.1416*r*r;
+        System.out.println("The area of circle is: "+area);
+        System.out.println(area);
+
+    }
+}
