1ef09d334c6df9d15497ed1514716de4660868d0
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-08-29 01:33:07 +0600

    yes

--- SOFT CLS/CST_4Study_opy.java
+++ SOFT CLS/CST_4Study_opy.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CST_4Study{
+public class CST_4Study_opy{
     public static void main(String[]args){
         // int c;
         Scanner c = new Scanner(System.in);
