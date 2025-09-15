b5b593940ba259279b0dc8855c6fe2b6c5dcd8b0
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-09-15 06:36:57 +0600

    learing

---
+++ Revitionone.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Revitionone {
+    public static void main(String[]args){
+        int choice;
+        System.out.println("BANKING PROGRAM");
+        System.out.println("1.Deposit Money");
+        System.out.println("2.Withdow Money");
+        System.out.println("3.Check Balance");
+        System.out.println("4.Exit");
+        System.out.println("*****************");
+        System.out.println("Enter you choice (1 to 4): ");
+        Scanner scanner = new Scanner(System.in);
+        choice = scanner.nextInt();
+    }
+}
