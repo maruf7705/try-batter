d0c2988d7ffb515c4af78ca9cf07d742c456a30b
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-09-15 06:43:17 +0600

    update

---
+++ Revitiononeone.java
@@ -0,0 +1,10 @@
+public class Revitiononeone {
+    public static void main(String[]args){
+        System.out.println("Hello Java");
+        System.out.println("Maruf Is the Best");
+
+        String Name = "Maruf";
+        int age = 20;
+        System.out.println("My name is: "+Name+"And my age is: "+age);
+    }
+}
